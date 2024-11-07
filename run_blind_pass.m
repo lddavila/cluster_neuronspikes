908f222d30e880d9ea7c4321a57d27eb6a4548eb
lddavila <lddavila@miners.utep.edu>
2024-11-07 13:49:08 -0700

    Added different min z score thresholds

--- run_blind_pass.m
+++ run_blind_pass.m
@@ -34,3 +34,91 @@ for i=1:length(subfolder_names)
     what_is_precomputed = [""];
     what_is_precomputed = run_entire_clustering_algorithm_ver_2(scale_factor,dir_with_channel_recordings,min_z_score,num_dps,timestamps_dir,precomputed_dir,what_is_precomputed,min_threshold);
 end
+
+%% run the blind pass with a different min_z_score (cut threshold) of 5
+precomputed_dir = "D:\spike_gen_data\Recordings By Channel Precomputed";
+dir_of_timestamps = "D:\spike_gen_data\Recordings By Channel Timestamps";
+for i=1:length(subfolder_names)
+    close all;
+    clc;
+    scale_factor = -1;
+    dir_with_channel_recordings = recording_dir + "\"+string(subfolder_names{i});
+    min_z_score = 5;
+    min_threshold = 20;
+    num_dps = 60;
+    timestamps_dir = dir_of_timestamps+"\"+string(subfolder_names{i});
+    create_z_score_matrix = 0;
+    precomputed_dir_current = precomputed_dir +"\"+string(subfolder_names{i});
+    what_is_recomputed = ["z_score","mean_and_std"];
+    what_is_precomputed = run_entire_clustering_algorithm_ver_2(scale_factor,dir_with_channel_recordings,min_z_score,num_dps,timestamps_dir,precomputed_dir,what_is_precomputed,min_threshold);
+end
+%% run the blind pass with a different min_z_score (cut threshold) of 6
+precomputed_dir = "D:\spike_gen_data\Recordings By Channel Precomputed";
+dir_of_timestamps = "D:\spike_gen_data\Recordings By Channel Timestamps";
+for i=1:length(subfolder_names)
+    close all;
+    clc;
+    scale_factor = -1;
+    dir_with_channel_recordings = recording_dir + "\"+string(subfolder_names{i});
+    min_z_score = 6;
+    min_threshold = 20;
+    num_dps = 60;
+    timestamps_dir = dir_of_timestamps+"\"+string(subfolder_names{i});
+    create_z_score_matrix = 0;
+    precomputed_dir_current = precomputed_dir +"\"+string(subfolder_names{i});
+    what_is_recomputed = ["z_score","mean_and_std"];
+    what_is_precomputed = run_entire_clustering_algorithm_ver_2(scale_factor,dir_with_channel_recordings,min_z_score,num_dps,timestamps_dir,precomputed_dir,what_is_precomputed,min_threshold);
+end
+%% run the blind pass with a different min_z_score (cut threshold) of 7
+precomputed_dir = "D:\spike_gen_data\Recordings By Channel Precomputed";
+dir_of_timestamps = "D:\spike_gen_data\Recordings By Channel Timestamps";
+for i=1:length(subfolder_names)
+    close all;
+    clc;
+    scale_factor = -1;
+    dir_with_channel_recordings = recording_dir + "\"+string(subfolder_names{i});
+    min_z_score = 7;
+    min_threshold = 20;
+    num_dps = 60;
+    timestamps_dir = dir_of_timestamps+"\"+string(subfolder_names{i});
+    create_z_score_matrix = 0;
+    precomputed_dir_current = precomputed_dir +"\"+string(subfolder_names{i});
+    what_is_recomputed = ["z_score","mean_and_std"];
+    what_is_precomputed = run_entire_clustering_algorithm_ver_2(scale_factor,dir_with_channel_recordings,min_z_score,num_dps,timestamps_dir,precomputed_dir,what_is_precomputed,min_threshold);
+end
+
+%% run the blind pass with a different min_z_score (cut threshold) of 15
+precomputed_dir = "D:\spike_gen_data\Recordings By Channel Precomputed";
+dir_of_timestamps = "D:\spike_gen_data\Recordings By Channel Timestamps";
+for i=1:length(subfolder_names)
+    close all;
+    clc;
+    scale_factor = -1;
+    dir_with_channel_recordings = recording_dir + "\"+string(subfolder_names{i});
+    min_z_score = 15;
+    min_threshold = 20;
+    num_dps = 60;
+    timestamps_dir = dir_of_timestamps+"\"+string(subfolder_names{i});
+    create_z_score_matrix = 0;
+    precomputed_dir_current = precomputed_dir +"\"+string(subfolder_names{i});
+    what_is_recomputed = ["z_score","mean_and_std"];
+    what_is_precomputed = run_entire_clustering_algorithm_ver_2(scale_factor,dir_with_channel_recordings,min_z_score,num_dps,timestamps_dir,precomputed_dir,what_is_precomputed,min_threshold);
+end
+
+%% run the blind pass with a different min_z_score (cut threshold) of 1
+precomputed_dir = "D:\spike_gen_data\Recordings By Channel Precomputed";
+dir_of_timestamps = "D:\spike_gen_data\Recordings By Channel Timestamps";
+for i=1:length(subfolder_names)
+    close all;
+    clc;
+    scale_factor = -1;
+    dir_with_channel_recordings = recording_dir + "\"+string(subfolder_names{i});
+    min_z_score = 5;
+    min_threshold = 1;
+    num_dps = 60;
+    timestamps_dir = dir_of_timestamps+"\"+string(subfolder_names{i});
+    create_z_score_matrix = 0;
+    precomputed_dir_current = precomputed_dir +"\"+string(subfolder_names{i});
+    what_is_recomputed = ["z_score","mean_and_std"];
+    what_is_precomputed = run_entire_clustering_algorithm_ver_2(scale_factor,dir_with_channel_recordings,min_z_score,num_dps,timestamps_dir,precomputed_dir,what_is_precomputed,min_threshold);
+end
