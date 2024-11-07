ca131ac6e84679e4d264b8260fb801575d9d3a2a
lddavila <lddavila@miners.utep.edu>
2024-11-07 14:43:10 -0700

    added other metrics for comparing ground truth

--- Utility Functions/compare_timestamps_to_ground_truth_ver_2.m
+++ Utility Functions/compare_timestamps_to_ground_truth_ver_2.m
@@ -2,7 +2,9 @@ function [table_of_info] = compare_timestamps_to_ground_truth_ver_2(ground_truth
 ground_truth_timestamps = get_corresponding_timestamp_for_each_unit(timestamps,ground_truth);
 accuracy = [];
 unit_id = [];
+overlap_with_unit = [];
 cluster_number = [];
+number_of_false_positives = [];
 
 for j=1:size(timestamps_of_clusters,2)
     timestamps_of_current_cluster = timestamps_of_clusters{j};
@@ -19,15 +21,16 @@ for j=1:size(timestamps_of_clusters,2)
 
         current_accuracy = number_of_timestamps_in_both/(length(in_unit_but_not_in_cluster) +number_of_timestamps_in_both + length(false_positives)) * 100;
 
-
+        overlap_with_unit = [overlap_with_unit;((number_of_timestamps_in_both/length(unit_timestamps))*100)];
         accuracy = [accuracy;current_accuracy];
         cluster_number = [cluster_number;j];
         unit_id = [unit_id;i];
+        number_of_false_positives = [number_of_false_positives;false_positives];
 
 
 
     end
     disp("Finished "+string(j)+"/"+size(timestamps_of_clusters,2));
 end
-table_of_info = table(accuracy,cluster_number,unit_id);
+table_of_info = table(cluster_number,unit_id,accuracy,overlap_with_unit,number_of_false_positives);
 end
