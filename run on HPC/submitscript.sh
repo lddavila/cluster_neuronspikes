00771f308058fcf29bf9e023a81fe0e46b29691b
lddavila@miners.utep.edu <lddavila@miners.utep.edu>
2025-02-11 15:51:14 -0700

    removed unnecessary line from the submit script

--- run on HPC/submitscript.sh
+++ run on HPC/submitscript.sh
@@ -3,6 +3,5 @@
 #SBATCH -p general 
 #SBATCH -o output.txt 
 #SBATCH -e error.txt 
-source /opt/intel/oneapi/setvars.sh -ofi_internal=1 --force
 module load matlab/R2024b
 matlab run_overlap_checks_on_hpc -np 6
