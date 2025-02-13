cef7462a0a1b150d934173ec9e1dcabe8a42c15d
lddavila@miners.utep.edu <lddavila@miners.utep.edu>
2025-02-13 16:58:10 -0700

    fixed submit script

--- run on HPC/ss_run_create_figures_on_hpc.sh
+++ run on HPC/ss_run_create_figures_on_hpc.sh
@@ -4,4 +4,4 @@
 #SBATCH -o output.txt 
 #SBATCH -e error.txt 
 module load matlab/R2024b
-matlab -batch "create_cluster_plots_with_grades('/home/lddavila/data_to_be_copied_to_local_server','Best Clusters Min Overlap 5 Percent Plots','/home/lddavila/spike_gen_data/0_100Neuron300SecondRecordingWithLevel3Noise/initial_pass_results min z_score','/home/lddavila/spike_gen_data/0_100Neuron300SecondRecordingWithLevel3Noise/initial_pass min z_score');exit;"
+matlab -batch "create_cluster_plots_with_grades('/home/lddavila/data_to_be_copied_to_local_server','/home/lddavila/data_to_be_copied_to_local_server/Blind Pass Overlap Min Overlap Threshold 5','Best Clusters Min Overlap 5 Percent Plots','/home/lddavila/spike_gen_data/0_100Neuron300SecondRecordingWithLevel3Noise/initial_pass_results min z_score','/home/lddavila/spike_gen_data/0_100Neuron300SecondRecordingWithLevel3Noise/initial_pass min z_score');exit;"
