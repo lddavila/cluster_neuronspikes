a61fb78db3b4b9785a3e86cdf852683772112543
lddavila@miners.utep.edu <lddavila@miners.utep.edu>
2025-02-14 13:18:07 -0700

    adjusted bp to 10 percent

--- run on HPC/run_overlap_checks_for_blind_pass.m
+++ run on HPC/run_overlap_checks_for_blind_pass.m
@@ -4,7 +4,7 @@ addpath(genpath(pwd));
 cd(home_dir);
 varying_z_scores = [4,5,6,7,8,9];
 tetrodes_to_check = strcat("t",string(1:285));
-min_overlap_percentage = 5;
+min_overlap_percentage = 10;
 debug = 0;
 grades_that_matter = [2,31,30,32,8,28,29,33,34,9,35,36,37,38,40,41];
 names_of_grades =["CV (2)","Amp. (31)","Hist. Sym. (30)","R-Wire Amp(32)","TM(8)","CL. Skew(28)","TM New()","Chance Of M.U.A(33)","Min B-Dist From M.U.A(34)","Min B-Dist To Neighbor(9)","TM Cluster Level(35)","TM Rep Wire Level(36)","Min Bhat Best Dim","Best Dim","SNR","burst ratio(41)"] ;
