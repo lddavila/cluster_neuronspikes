c197a94a3c00b38ea3d8b075d1270388d6abed1d
lddavila@miners.utep.edu <lddavila@miners.utep.edu>
2025-02-13 17:07:03 -0700

    fixed loading bug

--- run on HPC/create_cluster_plots_with_grades.m
+++ run on HPC/create_cluster_plots_with_grades.m
@@ -2,13 +2,13 @@ function [] = create_cluster_plots_with_grades(parent_dir_of_data_saving,data_to
 home_dir = cd("..");
 addpath(genpath(pwd));
 cd(home_dir);
-load(data_to_load,"best_appearences_of_cluster_from_blind_pass")
+load(data_to_load,"best_appearences_of_cluster")
 
 %'VariableNames',["Tetrode","Cluster","Z Score","SNR","Overlap Percentage","idx of its location in arrays"]);
 home_dir = cd(parent_dir_of_data_saving);
 dir_for_figures_to_be_saved_to = create_a_file_if_it_doesnt_exist_and_ret_abs_path(name_of_dir_to_save_files_to);
 refinement_pass = false;
 cd(dir_for_figures_to_be_saved_to)
-regrade_best_rep_and_plot_output_hpc(gen_dir_with_grades,gen_dir_of_outputs,best_appearences_of_cluster_from_blind_pass,refinement_pass)
+regrade_best_rep_and_plot_output_hpc(gen_dir_with_grades,gen_dir_of_outputs,best_appearences_of_cluster,refinement_pass)
 cd(home_dir);
 end
