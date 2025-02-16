13fba1ed9b0b21945a3b5b4ec434733f6d92429f
lddavila@miners.utep.edu <lddavila@miners.utep.edu>
2025-02-16 16:56:48 -0700

    fixed the saving for grading

--- run on HPC/run_grading_script_on_blind_pass.m
+++ run on HPC/run_grading_script_on_blind_pass.m
@@ -9,11 +9,11 @@ config = config.spikesort;
 debug = 0;
 varying_z_scores = [3,4,5,6,7,8,9];
 home_dir = cd(parent_dir_to_save_grades_to);
-dir_all_new_grades_will_be_saved_to = create_a_file_if_it_doesnt_exist_and_ret_abs_path(name_of_recording);
+dir_all_new_grades_will_be_saved_to = create_a_file_if_it_doesnt_exist_and_ret_abs_path(fullfile(parent_dir_to_save_grades_to,name_of_recording));
 cd(dir_all_new_grades_will_be_saved_to);
 for i=1:length(varying_z_scores)
     dir_with_output = fullfile(dir_with_data,"initial_pass_results min z_score"+string(varying_z_scores(i)));
-    dir_to_save_grades_to = "initial_pass min z_score "+string(varying_z_scores(i))+" grades";
+    dir_to_save_grades_to = fullfile(pwd,"initial_pass min z_score "+string(varying_z_scores(i))+" grades");
     dir_to_save_grades_to = create_a_file_if_it_doesnt_exist_and_ret_abs_path(dir_to_save_grades_to);
     list_of_tetrodes = strcat("t",string(1:285));
     dir_with_timestamps_and_rvals =  fullfile(dir_with_data,"initial_pass min z_score"+string(varying_z_scores(i)));
