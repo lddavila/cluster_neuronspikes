663abc043c9bb990cd21ea63d1338c79b7afce10
lddavila@miners.utep.edu <lddavila@miners.utep.edu>
2025-02-17 20:48:59 -0700

    fixed the nonsense

--- run on HPC/ss_generate_all_cluster_plots_for_bp.sh
+++ run on HPC/ss_generate_all_cluster_plots_for_bp.sh
@@ -4,4 +4,4 @@
 #SBATCH -o output.txt 
 #SBATCH -e error.txt 
 module load matlab/R2024b
-matlab -batch "generate_all_cluster_plots_for_bp('/home/lddavila/data_to_be_copied_to_local_server','Best Clusters Min Overlap 10 Percent Plots','/home/lddavila/spike_gen_data/0_100Neuron300SecondRecordingWithLevel3Noise/initial_pass_results min z_score','/home/lddavila/data_to_be_copied_to_local_server/0_100Neuron300SecondRecordingWithLevel3Noise/initial_pass min z_score');exit;"
+matlab -batch "generate_all_cluster_plots_for_bp('/home/lddavila/data_to_be_copied_to_local_server','Best Clusters Min Overlap 15 Percent Plots','/home/lddavila/spike_gen_data/0_100Neuron300SecondRecordingWithLevel3Noise/initial_pass_results min z_score','/home/lddavila/data_to_be_copied_to_local_server/0_100Neuron300SecondRecordingWithLevel3Noise/initial_pass min z_score');exit;"
