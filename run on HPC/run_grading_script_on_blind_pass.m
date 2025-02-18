43319feecadcdd30c56d043d3b328eb10f90f4ed
lddavila@miners.utep.edu <lddavila@miners.utep.edu>
2025-02-18 15:47:33 -0700

    moved par for to lower level

--- run on HPC/run_grading_script_on_blind_pass.m
+++ run on HPC/run_grading_script_on_blind_pass.m
@@ -12,7 +12,7 @@ number_of_z_scores_to_check = length(varying_z_scores);
 home_dir = cd(parent_dir_to_save_grades_to);
 dir_all_new_grades_will_be_saved_to = create_a_file_if_it_doesnt_exist_and_ret_abs_path(fullfile(parent_dir_to_save_grades_to,name_of_recording));
 cd(dir_all_new_grades_will_be_saved_to);
-parfor i=1:length(varying_z_scores)
+for i=1:length(varying_z_scores)
     dir_with_output = fullfile(dir_with_data,"initial_pass_results min z_score"+string(varying_z_scores(i)));
     dir_to_save_grades_to = fullfile(pwd,"initial_pass min z_score "+string(varying_z_scores(i))+" grades");
     dir_to_save_grades_to = create_a_file_if_it_doesnt_exist_and_ret_abs_path(dir_to_save_grades_to);
