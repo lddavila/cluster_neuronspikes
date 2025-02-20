d629e95e257b6123e65a1213186670a1255ee85a
lddavila@miners.utep.edu <lddavila@miners.utep.edu>
2025-02-19 18:04:45 -0700

    fixed file path

--- run on HPC/ss_classify_clusters_as_neurons_based_on_overlap_with_unit.sh
+++ run on HPC/ss_classify_clusters_as_neurons_based_on_overlap_with_unit.sh
@@ -4,4 +4,4 @@
 #SBATCH -o output.txt 
 #SBATCH -e error.txt 
 module load matlab/R2024b
-matlab -batch "run_overlap_checks_for_blind_pass('/home/lddavila/data_to_be_copied_to_local_server','/home/lddavila/spike_gen_data/0_100Neuron300SecondRecordingWithLevel3Noise/initial_pass_results min z_score','/home/lddavila/spike_gen_data/0_100Neuron300SecondRecordingWithLevel3Noise/initial_pass min z_score','/home/lddavila/ground_truth','/home/lddavila/timestamps/0_100Neuron300SecondRecordingWithLevel3Noise','/home/lddavila/spike_gen_data/0_100Neuron300SecondRecordingWithLevel3Noise'); exit;"
+matlab -batch "run_overlap_checks_for_blind_pass('/home/lddavila/ground_truth/Recording By Channel Ground Truth',0.0004,'/home/lddavila/spike_gen_data/0_100Neuron300SecondRecordingWithLevel3Noise','/home/lddavila/timestamps/Recordings By Channel Timestamps/0_100Neuron300SecondRecordingWithLevel3Noise','/home/lddavila/data_to_be_copied_to_local_server'); exit;"
