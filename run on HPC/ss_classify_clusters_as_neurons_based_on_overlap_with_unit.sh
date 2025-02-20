ec461f6a6e082d278d85110e4b367943f5328f23
lddavila@miners.utep.edu <lddavila@miners.utep.edu>
2025-02-19 18:48:00 -0700

    fixed the wrong file in function call

--- run on HPC/ss_classify_clusters_as_neurons_based_on_overlap_with_unit.sh
+++ run on HPC/ss_classify_clusters_as_neurons_based_on_overlap_with_unit.sh
@@ -4,4 +4,4 @@
 #SBATCH -o output.txt 
 #SBATCH -e error.txt 
 module load matlab/R2024b
-matlab -batch "generate_contamination_table('D:\spike_gen_data\Recording By Channel Ground Truth\0_100Neuron300SecondRecordingWithLevel3Noise.h5.mat',0.0004,'/home/lddavila/spike_gen_data/0_100Neuron300SecondRecordingWithLevel3Noise','/home/lddavila/timestamps/Recordings By Channel Timestamps/0_100Neuron300SecondRecordingWithLevel3Noise','/home/lddavila/data_to_be_copied_to_local_server'); exit;"
+matlab -batch "generate_contamination_table('/home/lddavila/ground_truth/Recording By Channel Ground Truth/0_100Neuron300SecondRecordingWithLevel3Noise.h5.mat',0.0004,'/home/lddavila/spike_gen_data/0_100Neuron300SecondRecordingWithLevel3Noise','/home/lddavila/timestamps/Recordings By Channel Timestamps/0_100Neuron300SecondRecordingWithLevel3Noise','/home/lddavila/data_to_be_copied_to_local_server'); exit;"
