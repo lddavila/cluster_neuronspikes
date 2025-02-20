bee199d9c3a850bebc1f21c392e5de818c7a5067
lddavila@miners.utep.edu <lddavila@miners.utep.edu>
2025-02-19 18:21:15 -0700

    fixed filepaths

--- Utility Functions/load_ground_truth_into_data.m
+++ Utility Functions/load_ground_truth_into_data.m
@@ -2,7 +2,7 @@ function [ground_truth_indexes] = load_ground_truth_into_data(dir_with_ground_tr
 list_of_neuron_files = strtrim(string(ls(fullfile(dir_with_ground_truth,"*.mat"))));
 ground_truth_indexes = cell(1,size(list_of_neuron_files,1));
 for i=1:size(list_of_neuron_files,1)
-    disp(fullfile(dir_with_ground_truth, list_of_neuron_files(i)));
+    %disp(fullfile(dir_with_ground_truth, list_of_neuron_files(i)));
 	ground_truth_indexes{i} = importdata(fullfile(dir_with_ground_truth, list_of_neuron_files(i)));
     for j=1:size(ground_truth_indexes{i},2)
         ground_truth_indexes{i}{j} = ground_truth_indexes{i}{j} +1;
