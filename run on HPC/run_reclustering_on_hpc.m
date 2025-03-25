6324d16bb4864c70fcf72cf370335513769423f8
lddavila@miners.utep.edu <lddavila@miners.utep.edu>
2025-03-24 19:35:21 -0600

    fixed file paths

--- run on HPC/run_reclustering_on_hpc.m
+++ run on HPC/run_reclustering_on_hpc.m
@@ -2,7 +2,9 @@ function [] = run_reclustering_on_hpc()
 home_dir = cd("..");
 addpath(genpath(pwd));
 cd(home_dir);
-load(fullfile("home","lddavila","data_from_local_server","table_with_best_channels.mat"),"-mat","table_with_best_channels")
+disp("finished adding file paths")
+load(fullfile(".","home","lddavila","data_from_local_server","table_with_best_channels.mat"),"-mat","table_with_best_channels")
+disp("finished loading")
 recluster_with_ideal_dims(table_with_best_channels,choose_best_config());
 
 end
