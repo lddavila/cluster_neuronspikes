cca4629fab1ab7c946eefb9eab60c46d6e07ecc6
lddavila@miners.utep.edu <lddavila@miners.utep.edu>
2025-03-26 16:56:21 -0600

    reverted reclustering code and fixed grading script

--- run on HPC/get_grades_for_ideal_dims_pass.m
+++ run on HPC/get_grades_for_ideal_dims_pass.m
@@ -5,15 +5,15 @@ number_of_tetrodes = length(list_of_tetrodes);
 for i=1:length(list_of_tetrodes)
     % disp("Made it into the for loop")
     current_tetrode = list_of_tetrodes(i);
-    
-   
-    
+
+
+
     try
         ts_r_tvals_cc_struct = load(fullfile(dir_with_timestamps_and_rvals,current_tetrode+".mat"),"timestamps","r_tvals","cleaned_clusters");
         timestamps = ts_r_tvals_cc_struct.timestamps;
         r_tvals = ts_r_tvals_cc_struct.r_tvals;
         cleaned_clusters = ts_r_tvals_cc_struct.cleaned_clusters;
-        
+
         aligned_struct = load(fullfile(dir_with_results,current_tetrode+" aligned.mat"),"aligned");
         aligned = aligned_struct.aligned;
         % output_struct = load(fullfile(dir_with_results,current_tetrode+" output.mat"),"output");
@@ -36,10 +36,10 @@ for i=1:length(list_of_tetrodes)
     % disp(pwd)
     save(current_tetrode+" Grades.mat",'grades');
     % disp(pwd)
-    
-    
+
+
     disp(string(min_z_score)+" Finished "+string(number_of_tetrodes));
-    
+
 end
 cd(dir_to_begin_and_end_the_func_in);
 end
