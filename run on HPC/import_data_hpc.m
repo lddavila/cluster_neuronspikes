dfe5c67d4d5e7adc648d100615dc1d812054c9d7
lddavila@miners.utep.edu <lddavila@miners.utep.edu>
2025-04-20 18:49:06 -0600

    removed try catch and par for for debugging

--- run on HPC/import_data_hpc.m
+++ run on HPC/import_data_hpc.m
@@ -1,6 +1,6 @@
 function [grades,output,aligned,reg_timestamps_of_the_spikes,idx,failed_to_load] = import_data_hpc(dir_with_grades,dir_with_outputs,current_tetrode,refinement_pass)
 failed_to_load=false;
-try
+% try
 
     load(fullfile(dir_with_outputs,current_tetrode+" output.mat"),"output");
     load(fullfile(dir_with_outputs,current_tetrode+" aligned.mat"),"aligned");
@@ -32,17 +32,17 @@ try
     % end
     load(fullfile(dir_with_outputs,current_tetrode+" reg_timestamps_of_spikes.mat"),"reg_timestamps_of_the_spikes");
     idx = extract_clusters_from_output(output(:,1),output);
-catch ME
-    failed_to_load=true;
-    disp(ME.identifier)
-    disp(ME.message)
-    disp(ME.cause)
-    disp(ME.stack)
-    grades = NaN;
-    output = NaN;
-    aligned = NaN;
-    reg_timestamps_of_the_spikes = NaN;
-    idx = NaN;
-end
+% catch ME
+%     failed_to_load=true;
+%     disp(ME.identifier)
+%     disp(ME.message)
+%     disp(ME.cause)
+%     disp(ME.stack)
+%     grades = NaN;
+%     output = NaN;
+%     aligned = NaN;
+%     reg_timestamps_of_the_spikes = NaN;
+%     idx = NaN;
+% end
 
 end
