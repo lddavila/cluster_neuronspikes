6eb4a2fa99d2ed13348875f247ca2143c33db64f
lddavila@miners.utep.edu <lddavila@miners.utep.edu>
2025-04-21 02:44:41 -0600

    updated grades

--- run on HPC/draw_elipse_templates.m
+++ run on HPC/draw_elipse_templates.m
@@ -1,4 +1,10 @@
-% Define the parameters
+config = spikesort_config;
+if config.ON_HPC
+    dir_to_save_templates_to =config.TEMPLATE_CLUSTER_FP_ON_HPC;
+else
+    dir_to_save_templates_to =config.TEMPLATE_CLUSTER_FP;
+end
+%First Draw The Circle
 x_center = 0;
 y_center = 0;
 a = 1; % Semi-major axis
@@ -17,35 +23,47 @@ x_rot = rotated_coords(1, :);
 y_rot = rotated_coords(2, :);
 
 
-%% Plot the ellipse
+% Now plot and save the circle picture
 close all;
 figure;
 k = boundary(x',y',0);
 f = fill(x(k), y(k),'k');
-
 axis equal;
 axis off;
-saveas(f,"D:\cluster_neuronspikes\Shape Template PNGs\circle.png")
-%%
-figure;
-k = boundary(x_rot',y_rot',0);
-f = fill(x_rot(k),y_rot(k),'k');
-axis equal
-axis off;
-saveas(f,"D:\cluster_neuronspikes\Shape Template PNGs\Left To Right Positive Elipse.png");
-
-
-
-%%
+saveas(f,dir_to_save_templates_to(5))
 close all;
-array_of_fp =["D:\cluster_neuronspikes\Shape Template PNGs\Horizontal Elipse.png"
-"D:\cluster_neuronspikes\Shape Template PNGs\Left To Right Negative Elipse.png"
-"D:\cluster_neuronspikes\Shape Template PNGs\Left To Right Positive Elipse.png"
-"D:\cluster_neuronspikes\Shape Template PNGs\Vertical Elipse.png"
-"D:\cluster_neuronspikes\Shape Template PNGs\circle.png"];
-the_cluster_image = imread(array_of_fp(5));
-grayImage = rgb2gray(the_cluster_image);
-binary_image = imbinarize(grayImage);
-disp("num pixels:"+string(size(binary_image)));
+
+% now draw the elipse templates
+rotation_angles =[0,(3*pi)/4, pi/4,pi/2] ;
+for i=1:size(rotation_angles,2)
+    x_center = 0;
+    y_center = 0;
+    a = 3; % Semi-major axis
+    b = 1; % Semi-minor axis
+    theta = linspace(0, 4*pi, 100);
+    phi = rotation_angles(i); % Rotation angle (in radians)
+
+
+    % Parametric equations for the ellipse
+    x = x_center + a * cos(theta);
+    y = y_center + b * sin(theta);
+
+    R = [cos(phi) -sin(phi); sin(phi) cos(phi)];
+    rotated_coords = R * [x; y];
+    x_rot = rotated_coords(1, :);
+    y_rot = rotated_coords(2, :);
+    figure;
+    k = boundary(x_rot',y_rot',0);
+    f = fill(x_rot(k),y_rot(k),'k');
+    axis equal
+    axis off;
+    saveas(f,dir_to_save_templates_to(i));
+    close all;
+end
+
+
+
+
+
 
 
