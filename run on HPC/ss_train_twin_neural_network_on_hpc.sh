9c27a5cd01abd28f3ec7a7099666078d7d68e547
lddavila@miners.utep.edu <lddavila@miners.utep.edu>
2025-05-17 18:37:33 -0600

    conf for hpc run and changed output in ss

--- run on HPC/ss_train_twin_neural_network_on_hpc.sh
+++ run on HPC/ss_train_twin_neural_network_on_hpc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 #SBATCH -n 40 
 #SBATCH -p general 
-#SBATCH -o output_2.txt 
-#SBATCH -e error_2.txt 
+#SBATCH -o output.txt 
+#SBATCH -e error.txt 
 module load matlab/R2024b
 matlab -batch "train_twin_neural_network_on_hpc();exit;"
