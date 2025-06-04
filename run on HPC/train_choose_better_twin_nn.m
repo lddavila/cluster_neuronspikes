58579402e5a25fbd0e5cfe2a71c03d3d510edd7f
lddavila@miners.utep.edu <lddavila@miners.utep.edu>
2025-06-03 23:29:42 -0600

    removed parfor for debug

--- run on HPC/train_choose_better_twin_nn.m
+++ run on HPC/train_choose_better_twin_nn.m
@@ -48,7 +48,7 @@ number_of_permutations_to_try = size(learning_rates_to_try,2) * size(number_of_l
 
 
 
-parfor i=1:size(number_of_layers_to_try,2)
+for i=1:size(number_of_layers_to_try,2)
     layers = number_of_layers_to_try(i);
     neurons = 1;
     for k=1:size(learning_rates_to_try,2)
