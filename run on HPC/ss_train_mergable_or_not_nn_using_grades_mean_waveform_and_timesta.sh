72e0a09a76ba99b71da52e5fe597ad7d9a410b8c
lddavila@miners.utep.edu <lddavila@miners.utep.edu>
2025-06-17 18:35:41 -0600

    added submit script

---
+++ run on HPC/ss_train_mergable_or_not_nn_using_grades_mean_waveform_and_timesta.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+#SBATCH -n 40 
+#SBATCH -p medium 
+#SBATCH -o output.txt 
+#SBATCH -e error.txt 
+module load matlab/R2024b
+matlab -batch "train_mergable_or_not_nn_using_grades_mean_waveform_and_timesta(();exit;"
