84a171627f949e8e9af7c8c44b86d8541335cfbb
lddavila@miners.utep.edu <lddavila@miners.utep.edu>
2025-06-25 17:13:21 -0600

    fixed config to run on cory's hpc

--- clustering-master/conf/spikesort_config.m
+++ clustering-master/conf/spikesort_config.m
@@ -68,7 +68,7 @@ config.GENRIC_DIR_WITH_OUTPUTS = "D:\spike_gen_data\Recordings By Channel Precom
 config.DIR_TO_SAVE_RECLUSTERING_TO ="D:\spike_gen_data\Recordings By Channel Precomputed\0_100Neuron300SecondRecordingWithLevel3Noise Refinement Pass " + string(config.NUM_DIMS_TO_USE_FOR_RECLUSTERING) +" Channels";
 config.GT_FP = "D:\spike_gen_data\Recording By Channel Ground Truth\0_100Neuron300SecondRecordingWithLevel3Noise.h5.mat";
 
-config.ON_HPC = contains(pwd,"lddavila","IgnoreCase",true);
+config.ON_HPC = contains(pwd,"lddavila","IgnoreCase",true)|| contains(pwd,"cnheaton","IgnoreCase",true) ; 
 config.NUM_DIMS_TO_USE_FOR_RECLUSTERING_ON_HPC = [2 3 4 5 6 7];
 config.DIR_TO_SAVE_RECLUSTERING_TO_ON_HPC =fullfile("/home","lddavila","Reclusted Passs 0_100_3","Refinement Pass " + string(config.NUM_DIMS_TO_USE_FOR_RECLUSTERING_ON_HPC) +" Channels");
 config.BLIND_PASS_DIR_PRECOMPUTED_ON_HPC = fullfile("/home","lddavila","spike_gen_data","0_100Neuron300SecondRecordingWithLevel3Noise"); %the parent directory where the blind pass precomputed info is saved (things like spikes per channel and std per channel)
@@ -104,7 +104,7 @@ config.DIR_TO_SAVE_CLUSTER_IMAGE_PNGS_TO ="D:\cluster_neuronspikes\Data\limited_
 config.DIR_TO_SAVE_CLUSTER_IMAGE_PNGS_TO_ON_HPC ="/scratch/lddavila/data_from_local_machine/100_by_100_cluster_pngs";
 
 config.FP_TO_TABLE_OF_ALL_BP_CLUSTERS= "D:\cluster_neuronspikes\Data\final_overlap_table\final_table_with_overlap_only_neurons.mat";
-config.FP_TO_TABLE_OF_ALL_BP_CLUSTERS_ON_HPC = "/scratch/lddavila/data_from_local_machine/final_overlap_table/final_table_with_overlap_only_neurons.mat";
+config.FP_TO_TABLE_OF_ALL_BP_CLUSTERS_ON_HPC = "/scratch/cnheaton/data_from_local_machine/final_overlap_table/final_table_with_overlap_only_neurons.mat";
 
 %
 config.WHICH_NEURAL_NET = "complex choose better";
@@ -182,7 +182,7 @@ config.FP_TO_PREDICTING_ACCURACY_ON_GRADES_NN  = "D:\cluster_neuronspikes\Predic
 config.FP_TO_PREDICTING_ACCURACY_ON_GRADES_NN_ON_HPC = "/scratch/lddavila/cluster_neuronspikes/Predict_accuracy_cat_using_grades_nn/accuracy score 0.86357number of acc cats 3 num layers 6 num neurons per layer40 grades neural network.mat";
 
 config.FP_TO_COMPLEX_MERGE_OR_DONT_NN = "D:\cluster_neuronspikes\Data\06_17_2025_complex_merge_nn_with_overlap\accuracy score 0.99367 num layers 1 num neurons per layer15 complex merge checker.mat";
-config.FP_TO_COMPLEX_MERGE_OR_DONT_NN_ON_HPC = "";
+config.FP_TO_COMPLEX_MERGE_OR_DONT_NN_ON_HPC = "/scratch/cnheaton/data_from_local_machine/06_18_2025_complex_choose_better/accuracy score 0.85031 num layers 13 num neurons per layer10 complex choose better.mat'";
 
 config.DIR_TO_STATUS_FILE = "";
 config.FP_TO_STATUS_FILE = "";
---
+++ run on HPC/run_test_bubble_sort_on_overlapping_clusters_general.m
@@ -0,0 +1,13 @@
+function run_test_bubble_sort_on_overlapping_clusters_general()
+home_dir = cd("..");
+addpath(genpath(pwd));
+cd(home_dir);
+
+config = spikesort_config();
+if config.ON_HPC
+    blind_pass_table = config.FP_TO_TABLE_OF_ALL_BP_CLUSTERS_ON_HPC;
+else
+    blind_pass_table = config.FP_TO_TABLE_OF_ALL_BP_CLUSTERS;
+end
+test_bubble_sort_choose_better_on_array_of_overlapping_clusters({blind_pass_table},config,"/scratch/lddavila/data_to_copy_to_local_machine/06_25_2025_general_choose_better_test");
+end
