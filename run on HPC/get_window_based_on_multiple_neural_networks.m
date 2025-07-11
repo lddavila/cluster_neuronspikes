d86ab15c4bb2ceefa89af67a3b83e6f4680d7866
lddavila@miners.utep.edu <lddavila@miners.utep.edu>
2025-07-10 20:03:57 -0600

    fixed improper function call

--- run on HPC/get_window_based_on_multiple_neural_networks.m
+++ run on HPC/get_window_based_on_multiple_neural_networks.m
@@ -7,7 +7,7 @@ all_window_end = nan(size(blind_pass_table,1),1);
 parfor i=1:size(blind_pass_table,1)
     %for every sample in the blind_pass_table we must try to minimize the window where its true accuracy might exist
     beginning_time = tic;
-    [all_window_beginning(i),all_window_end(i)]= run_all_neural_networks(neural_networks_array,presorted_table,i,blind_pass_table,cluster_size_col,mean_waveform_array,grades_array,config);
+    [all_window_beginning(i),all_window_end(i)]= run_all_choose_better_neural_networks(neural_networks_array,presorted_table,i,blind_pass_table,cluster_size_col,mean_waveform_array,grades_array,config);
     end_time = toc(beginning_time);
     
     print_status_iter_message("get_window_based_on_multiple_neural_networks.m",i,size(blind_pass_table,1));
